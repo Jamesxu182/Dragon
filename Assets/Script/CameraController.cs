dcd3df80d17046fbd48e8537b38ecd65e954b26a
Yifan Xu <Yifan Xu>
2017-04-15 09:28:19 +0100

    add .gitignore

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -13,22 +13,28 @@ public class CameraController : MonoBehaviour {
     private Vector3 movement_target;
     private Vector3 relative_movement_target;
 
-    private Vector3 init_position;
+    private Vector3 view_target;
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(this.movement_target, 0.1f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(this.view_target + transform.forward * 1.0f, 0.01f);
     }
 
     // Use this for initialization
     void Start () {
-        this.init_position = transform.position;
         this.relative_movement_target = Vector3.zero;
         this.movement_target = transform.position;
-	}
+        this.view_target = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        Cursor.visible = false;
+
+
         float movement_h = Input.GetAxis("Movement Horizontal");
         float movement_v = Input.GetAxis("Movement Vertical");
 
@@ -39,26 +45,28 @@ public class CameraController : MonoBehaviour {
         movement_v += Mathf.Abs(ps_movement_v) > 0.05f ? ps_movement_v : 0.0f;
 
         Vector3 add_relative_movement_target = new Vector3(movement_h * movement_speed * Time.deltaTime, 0, movement_v * movement_speed * Time.deltaTime);
-        if(!add_relative_movement_target.Equals(Vector3.zero))
-        {
-            this.relative_movement_target += add_relative_movement_target;
-            this.movement_target = transform.TransformPoint(relative_movement_target);
-        }
-
-        else
-        {
-            this.relative_movement_target = this.movement_target - transform.position;
-        }
-
 
         if(isEnableLerp)
         {
+            if (!add_relative_movement_target.Equals(Vector3.zero))
+            {
+                this.relative_movement_target += add_relative_movement_target;
+                this.movement_target = transform.TransformPoint(relative_movement_target);
+            }
+
+            else
+            {
+                this.relative_movement_target += add_relative_movement_target;
+                this.movement_target = transform.TransformPoint(relative_movement_target);
+            }
+
             float smoothRate = Mathf.Clamp(movment_lerp_rate * Time.deltaTime, 0.0f, 1.0f);
             transform.position = Vector3.Lerp(transform.position, this.movement_target, smoothRate);
         }
 
         else
         {
+            this.movement_target = transform.TransformPoint(relative_movement_target);
             transform.Translate(new Vector3(movement_h * movement_speed * Time.deltaTime, 0, movement_v * movement_speed * Time.deltaTime));
         }
 
@@ -72,14 +80,13 @@ public class CameraController : MonoBehaviour {
         float mouse_h = Input.GetAxis("Mouse Horizontal");
         float mouse_v = Input.GetAxis("Mouse Vertical");
 
-        Debug.Log("Mouse_V: " + mouse_v);
-        Debug.Log("Mouse_H: " + mouse_h);
-
         view_h += Mathf.Abs(ps_view_h) > 0.05f ? ps_view_h : 0.0f;
         view_v += Mathf.Abs(ps_view_v) > 0.05f ? ps_view_v : 0.0f;
 
         view_h += mouse_h;
         view_v += mouse_v;
+
+        this.view_target = transform.TransformPoint(new Vector3(0.0f, 0.0f, 0.0f));
 
         transform.RotateAround(transform.position, transform.right, view_v * view_speed * Time.deltaTime);
         transform.RotateAround(transform.position, transform.up, view_h * view_speed * Time.deltaTime);
